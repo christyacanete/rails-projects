35d2de33a2c83ad0bb129c52cdd5bf12540d4cb9
Christy Canete <christyacanete@gmail.com>
2024-11-05 13:01:59 +0800

    Add mysite project

---
+++ mysite/app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+// This file is used to include any JavaScript files needed for your application.
+// You can leave it empty for now if you don't need any JavaScript.
+
+//= require_tree .
+//= require_self
