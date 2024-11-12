eafa3d7b13aa392b64a1f026fac9dea949496f87
Christy Canete <christyacanete@gmail.com>
2024-11-13 00:32:52 +0800

    Added a signup form

--- book_review_app/config/routes.rb
+++ book_review_app/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  
+  get "/signup", to: "users#new"
  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
