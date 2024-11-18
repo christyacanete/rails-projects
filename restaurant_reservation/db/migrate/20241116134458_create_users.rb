4b8a5fc7421a7758c5ce7c31b961672869cc691d
Christy Canete <christyacanete@gmail.com>
2024-11-18 15:45:16 +0800

    Added rails db migrate on render file

--- restaurant_reservation/db/migrate/20241116134458_create_users.rb
+++ restaurant_reservation/db/migrate/20241116134458_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
-      t.string :role
+    unless table_exists?(:users)
+      create_table :users do |t|
+        t.string :name
+        t.string :email
+        t.string :password_digest
+        t.string :role
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
